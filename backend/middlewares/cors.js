dd69d8f97da557abf168353bc5f0dd0512da6b5e
vpanacheva <v_panacheva@mail.ru>
2023-09-26 18:15:24 +0500

    3.19

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -10,18 +10,23 @@ const allowedCors = [
 ];
 
 module.exports = (req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
   const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   res.header('Access-Control-Allow-Credentials', true);
+  // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
+    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
     res.header('Access-Control-Allow-Origin', origin);
   }
 
   if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    // разрешаем кросс-доменные запросы с этими заголовками
     res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
     return res.end();
   }
 
